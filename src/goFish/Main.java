2d79ed5ebfe226fcd43791b38ab27827600fcab1
Siphesande2000 <36502241+Siphesande2000@users.noreply.github.com>
2024-06-19 12:36:57 +0200

    Delete src/goFish directory

--- src/goFish/Main.java
+++
@@ -1,9 +0,0 @@
-package goFish;
-
-public class Main {
-	public static void main(String[] args) {
-		GUIWelcomeScreen startYourEngines = new GUIWelcomeScreen();
-		startYourEngines.setVisible(true);
-	}
-
-}
